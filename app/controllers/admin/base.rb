d8b73c05564ea5f7af743e026fe0f292b48e0ef6
kajiwara3 <kajiwara3@gmail.com>
2012-11-12 00:10:16 +0900

    add devise for user and administrator

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,4 +1,4 @@
 # coding: utf-8
 class Admin::Base < ApplicationController
-
+  layout "admin_application"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Quiz::Application.routes.draw do
+  devise_for :users
+  devise_for :administrators
+
   root to: "top#index"
   resource :users
 
   namespace :admin do
+    devise_for :administrators
     root to: "top#index"
     resources :administrators
   end
