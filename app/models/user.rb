4d539d81130b0fb6134ac705e4b5eed7ad30e5f4
kajiwara3 <kajiwara3@gmail.com>
2012-11-18 12:11:16 +0900

    ユーザー登録の際に、プライバシーポリシーへの同意確認を追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -5,6 +6,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :privacy_agree
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :gender_id, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :gender_id, :password, :password_confirmation, :remember_me, :privacy_agree
+  belongs_to :gender
+
+  validates :name, :email, :gender_id, :password, :privacy_agree, presence: true
+  validate :privacy_agree_validate?
+
+  def privacy_agree_validate?
+    logger.debug '================ 1'
+    logger.debug privacy_agree
+    errors.add(:privacy_agree, "プライバシーポリシーへの合意が必要です") if privacy_agree == "false"
+  end
 end
