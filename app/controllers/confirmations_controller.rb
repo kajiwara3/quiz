0bf755db654ffd3ce96f4faac2dff13609810aac
kajiwara3 <kajiwara3@gmail.com>
2012-11-19 20:46:54 +0900

    ユーザー登録時にメールでアクティベートした後の処理を追加した

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,6 @@
 class ConfirmationsController < Devise::ConfirmationsController
-  def show
-    redirect_to wellcome_path
+  def after_confirmation_path_for(resource_name, resource)
+    # mail send
+    wellcome_math
   end
 end
