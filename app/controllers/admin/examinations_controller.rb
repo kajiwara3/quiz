c96129af7e8e2f0d6453498e11acf005f1ce1c2f
kajiwara3 <kajiwara3@gmail.com>
2012-12-04 22:27:28 +0900

    examinations controllerに新規追加アクションを追加した

--- app/controllers/admin/examinations_controller.rb
+++ app/controllers/admin/examinations_controller.rb
@@ -1,11 +1,40 @@
 # coding: utf-8
 class Admin::ExaminationsController < Admin::Base
   before_filter :authenticate_admin_administrator!
+
+  def new
+    @examination = Examination.new
+  end
+
+  def create
+    @examination = Examination.new
+    @examination.assign_attributes params[:examination]
+    if @examination.save
+      return redirect_to [:admin, @examination], notice: '保存しました。'
+    end
+    flash[:error] = '問題を確認してください'
+    return render 'new'
+  end
+
   def index
     @examinations = Examination.order(:id).page(params[:page]).per(5)
   end
 
   def show
     @examination = Examination.find(params[:id])
+  end
+
+  def edit
+    @examination = Examination.find(params[:id])
+  end
+
+  def update
+    @examination = Examination.find(params[:id])
+    @examination.assign_attributes params[:examination]
+    if @examination.save
+      return redirect_to [:admin, @examination], notice: "更新しました"
+    end
+    flash[:error] = '問題を確認してください'
+    return render 'edit'
   end
 end
--- app/models/examination.rb
+++ app/models/examination.rb
@@ -1,3 +1,4 @@
 class Examination < ActiveRecord::Base
   has_many :questions, dependent: :destroy
+  validates :name, :entry_capasity, :passing_grade, :start_at, :end_at, presence: true
 end
