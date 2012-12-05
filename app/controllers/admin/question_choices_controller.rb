abfb447d9a0a3424c1d75d1d4727f12863cd8374
kajiwara3 <kajiwara3@gmail.com>
2012-12-05 22:34:44 +0900

    question_choices_controllerにcreateアクションを追加した

--- app/controllers/admin/question_choices_controller.rb
+++ app/controllers/admin/question_choices_controller.rb
@@ -18,4 +18,22 @@ class Admin::QuestionChoicesController < ApplicationController
     flash[:error] = '更新に失敗しました。'
     render 'show'
   end
+
+  def new
+    @question = Question.find(params[:question_id])
+    @question_choice = QuestionChoice.new
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @question_choice = QuestionChoice.new
+    @question_choice.assign_attributes params[:question_choice]
+    if @question_choice.valid?
+      @question.question_choices << @question_choice
+      return redirect_to [:admin, @question, @question_choice],
+        notice: '追加しました。'
+    end
+    flash[:error] = "以下の問題を確認してください。"
+    render 'new'
+  end
 end
--- app/models/question_choice.rb
+++ app/models/question_choice.rb
@@ -1,3 +1,4 @@
 class QuestionChoice < ActiveRecord::Base
   belongs_to :question
+  validates :caption, presence: true
 end
