9bef3be73392ebf7134d64b40e14aed210dacf00
kajiwara3 <kajiwara3@gmail.com>
2012-12-25 21:43:26 +0900

    選択肢の一覧表示を実装した

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,10 @@
 class QuestionsController < ApplicationController
   def index
     @examination = Examination.find_by_id(params[:examination_id])
-    @questions = Kaminari::paginate_array(@examination.questions).page(params[:page]).per(1)
+    @questions = Kaminari::paginate_array(@examination.questions).page(params[:page]).per(10)
+  end
+
+  def show
+    @question = Question.find_by_id(params[:id])
   end
 end
