948ed30266fc0ba5b17efb618a8aefc0a026eeb1
kajiwara3 <kajiwara3@gmail.com>
2013-01-07 11:59:44 +0900

    デバッグログの削除

--- app/controllers/answer_sheets_controller.rb
+++ app/controllers/answer_sheets_controller.rb
@@ -4,7 +4,6 @@ class AnswerSheetsController < ApplicationController
     # response Last-Modifiedヘッダ
     @answer_sheets = current_user.answer_sheets.page(params[:page]).per(5)
     if stale?(@user)
-      logger.debug "===================== <changed>"
       # 重たい処理
       # @answer_sheets = current_user.answer_sheets.page(params[:page]).per(5)
     end
