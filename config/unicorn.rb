0f7eea8f7d2781dc564d8d3668e09a43efb0c977
kajiwara3 <kajiwara3@gmail.com>
2013-01-16 21:10:41 +0900

    nginxとの連動用に、unicornの設定を修正した

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@
 worker_processes 2
 
 # ソケット経由で通信する
-listen File.expand_path('tmp/sockets/unicorn.sock', ENV['RAILS_ROOT'])
+listen File.expand_path('tmp/unicorn_prototypebeta.sock', ENV['RAILS_ROOT'])
 
 # ログ
 stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
