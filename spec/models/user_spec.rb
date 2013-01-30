d715f86a4c3d0524672acc23279e255d786bcf73
kajiwara3 <kajiwara3@gmail.com>
2013-01-30 22:19:55 +0900

    userモデルのテストを実装した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "name,emailを指定した場合" do
+    before do
+      @user = User.new(
+        name: "user",
+        email: "user@a.jp"
+      )
+    end
+    it "nameがセットされていること" do
+      @user.name.should == 'user'
+    end
+    it "emailがセットされていること" do
+      @user.email.should == 'user@a.jp'
+    end
+  end
+
+  context 'name emailをセットしなかった場合' do
+    before do
+      @user = User.new
+    end
+    it {@user.should_not be_valid}
+  end
+
+  context 'nameをセットした場合' do
+    before do
+      @user = User.new(
+                name: 'name'
+              )
+    end
+    it {@user.should_not be_valid}
+  end
+
+  context '必須項目をすべてをセットした場合' do
+    before do
+      @user = User.new(
+                name: 'test',
+                email: 'a@a.jp',
+                gender_id: '1',
+                privacy_agree: 'true',
+                password: 'password',
+                password_confirmation: 'password'
+              )
+    end
+    it {@user.should be_valid}
+  end
 end
