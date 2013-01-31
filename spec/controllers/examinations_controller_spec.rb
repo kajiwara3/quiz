f57cf130462ef63f31b392b12ce2f25209b395fe
kajiwara3 <kajiwara3@gmail.com>
2013-01-31 21:40:54 +0900

    examination controllerのテストを実装した

--- spec/controllers/examinations_controller_spec.rb
+++ spec/controllers/examinations_controller_spec.rb
@@ -1,5 +1,33 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe ExaminationsController do
+  login_user
 
+  before do
+    @test_before = Factory.create :examination_before
+    @test_in_service = Factory.create :examination_in_service
+    @test_end = Factory.create :examination_end
+
+    get 'index'
+  end
+
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
+
+    it "期間内のテストを返す" do
+      assigns[:examinations].should =~[@test_in_service]
+    end
+
+    it "期間前のテストは返さない" do
+      assigns[:examinations].should_not =~[@test_before]
+    end
+
+    it "期間後のテストは返さない" do
+      assigns[:examinations].should_not =~[@test_end]
+    end
+  end
 end
--- spec/factories/examinations.rb
+++ spec/factories/examinations.rb
@@ -1,6 +1,25 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :examination do
+  factory :examination_before, class: Examination do |e|
+    e.name 'examination_before'
+    e.entry_capasity '10'
+    e.passing_grade '80'
+    e.start_at 1.days.from_now
+    e.end_at 2.days.from_now
+  end
+  factory :examination_in_service, class: Examination do |e|
+    e.name 'examination_service'
+    e.entry_capasity '10'
+    e.passing_grade '80'
+    e.start_at 1.days.ago
+    e.end_at 2.days.from_now
+  end
+  factory :examination_end, class: Examination do |e|
+    e.name 'examination_end'
+    e.entry_capasity '10'
+    e.passing_grade '80'
+    e.start_at 2.days.ago
+    e.end_at 1.days.ago
   end
 end
