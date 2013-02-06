25a2595d32155d57185f46b24254552b10a948a3
kajiwara3 <kajiwara3@gmail.com>
2013-02-06 23:28:43 +0900

    shared_examples_forを追加した

--- spec/controllers/examinations_controller_spec.rb
+++ spec/controllers/examinations_controller_spec.rb
@@ -2,21 +2,32 @@
 require 'spec_helper'
 
 describe ExaminationsController do
-  login_user
+  shared_examples_for 'Shared Example Group' do
+    before do
+      p '============ shared_examples_for: test'
+    end
+    it 'test' do
+      p '============ test sample'
+    end
+  end
 
+  login_user
   before do
     @test_before = Factory.create :examination_before
     @test_in_service = Factory.create :examination_in_service
     @test_end = Factory.create :examination_end
-
     get 'index'
   end
 
-  describe "GET 'index'" do
+  context "GET 'index'" do
     it "should be successful" do
       get 'index'
       response.should be_success
     end
+  end
+
+  context '期間に応じて正しいオブジェクトを返す場合' do
+    it_should_behave_like 'Shared Example Group'
 
     it "期間内のテストを返す" do
       assigns[:examinations].should =~[@test_in_service]
